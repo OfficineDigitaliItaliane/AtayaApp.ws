d55b35796aff806792b2185009379421354ea176
Michel Capitanio <>
2018-01-10 09:49:12 +0100

    updated deploy scripts

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -3,8 +3,6 @@
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null -p45000 $SSH_USER@$EC2_HOST << EOF
       cd /home/ruah
       sudo su
-      docker-compose stop
-      docker-compose rm -f web
-      docker-compose pull web
-      docker-compose up -d
+      git pull 
+      pm2 reload ruah --update-env
 EOF
