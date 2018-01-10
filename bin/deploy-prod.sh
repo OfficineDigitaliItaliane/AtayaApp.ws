d62418e60d65d4a45c37303cf499186bbd03c5bf
Michel Capitanio <>
2018-01-10 10:02:31 +0100

    fix bitbucket pipeline file

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null -p45000 $SSH_USER@$EC2_HOST << EOF
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null -p45000 $SSH_PROD_USER@$PROD_HOST << EOF
       cd /home/ruah
       sudo su
       git pull 
-      pm2 reload ruah --update-env
+      pm2 reload ruah --update-env 
 EOF
