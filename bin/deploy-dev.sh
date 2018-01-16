aa2e2bb857761fa407fda141fbdd4dc9697df9fa
Michel Capitanio <>
2018-01-16 14:28:46 +0100

    updated deployment scripts

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null -p45000 $SSH_DEV_USER@$DEV_HOST << EOF
-      cd /home/ruah
+      cd /home/ruah/ruah.ws
       sudo su
       git pull 
       pm2 reload ruah --update-env 
--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ssh -vvv -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null -p45000 $SSH_PROD_USER@$PROD_HOST << EOF
-      cd /home/ruah
+      cd /home/ruah/ruah.ws
       sudo su
       git pull 
       pm2 reload ruah --update-env 
