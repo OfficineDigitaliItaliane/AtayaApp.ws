8fa20089445e803598e9eb1320585ee3377471a6
Giovanni Liboni <giovanni@liboni.net>
2018-03-05 11:08:01 +0100

    Updated src/api/cms.js: moved some api groups under cms group

--- src/api/cms.js
+++ src/api/cms.js
@@ -3,10 +3,20 @@ import Auth from '../../config/auth'
 
 const router = new Router();
 const auth = new Auth()
+const mediaApi = require('./media')
+const speakApi = require('./speak')
+const readApi = require('./read')
+const writeApi = require('./write')
+const understandApi = require('./understand')
 
 const user = require('../controllers/users')
 
 router.post('/login', user.login)
 router.post('/signup', user.signup)
+router.use('/media', mediaApi)
+router.use('/speak', speakApi)
+router.use('/read', readApi)
+router.use('/write', writeApi)
+router.use('/understand', understandApi)
 
 module.exports = router
--- src/api/index.js
+++ src/api/index.js
@@ -3,22 +3,10 @@ import Express from 'express'
 import path from 'path'
 const express = Express()
 const mockApi = require('./mock')
-const speakApi = require('./speak')
-const readApi = require('./read')
-const writeApi = require('./write')
-const understandApi = require('./understand')
 const cmsApi = require('./cms')
-const mediaApi = require('./media')
 
 express.use('/docs', Express.static(path.join(__dirname, '../../docs/')))
 express.use('/mock', mockApi)
-express.use('/speak', speakApi)
-express.use('/read', readApi)
-express.use('/write', writeApi)
-express.use('/understand', understandApi)
 express.use('/cms', cmsApi)
-
-express.use('/media', mediaApi)
-
 
 module.exports = express
