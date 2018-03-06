31acd53c2cea8929de55e0a00e28c9eb5f1dcf6e
Giovanni Liboni <giovanni@liboni.net>
2018-03-06 14:55:26 +0100

    Updated docs

--- src/api/docs/api-doc.js
+++ src/api/docs/api-doc.js
@@ -10,3 +10,12 @@
 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIsImlhdCI6MTUwNjUxODY2Nn0.9eauZc8PHzcRGH888fmwsgqeCXL0Ka27MgQx_n0Dacg"
  *     }
  */
+
+/**
+ * @apiDefine appAuthHeader
+ @apiHeader {String} X-API-KEY Api Key value.
+ * @apiHeaderExample {json} Header-Example:
+ *     {
+"X-API-KEY": "apiKey"
+ *     }
+ */
---
+++ src/api/docs/book-doc.js
@@ -0,0 +1,11 @@
+/**
+ * Created by giovanni on 01/10/17.
+ */
+
+/**
+ * @api {get} /api/cms/book/create Create book zip
+ * @apiGroup Book
+ * @apiUse authHeader
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ */
