798817630c527701aadab58b8e74cfa8d0c294bb
Giuseppe Zanini <giuseppe.zanini@mindtek.it>
2020-06-10 11:12:58 +0200

    Added media credits

--- src/models/speak.js
+++ src/models/speak.js
@@ -1,9 +1,10 @@
 //Define a schema
 import mongoose from 'mongoose'
+import {Media} from "./common.models"
 
 module.exports = new mongoose.Schema({
     unit_id: {type: Number, required: true},
     title: {type: String, required: true},
-    picture: {type: String, required: true},
-    audio: {type: String, required: true}
+    picture: {type: Media, required: true},
+    audio: {type: Media, required: true}
 }, { versionKey: false, collection: 'speak'})
