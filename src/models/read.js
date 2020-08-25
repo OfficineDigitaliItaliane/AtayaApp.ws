ba0c8e0445906e957f4410d34007ca3b30e0d5c1
Giuseppe Zanini <giuseppe.zanini@mindtek.it>
2020-08-25 15:35:52 +0200

    Added markers to read

--- src/models/read.js
+++ src/models/read.js
@@ -2,15 +2,22 @@
 import mongoose from 'mongoose'
 import {Media} from "./common.models"
 
+var Marker =  new mongoose.Schema({
+    x: {type: Number, required: true},
+    y: {type: Number, required: true},
+    id: {type: String, required: true} 
+});
+
 var readOption = new mongoose.Schema({
     body: {type: String, required: true},
     audio: {type: Media, required: true},
-    correct: {type: Boolean, required: true}
+    markerId: {type: String, required: true}
 });
 
 module.exports = new mongoose.Schema({
     unit_id: {type: Number, required: true},
     title: {type: String, required: true},
     picture: {type: Media, required: true},
-    options: {type: [readOption], required: true}
+    options: {type: [readOption], required: true},
+    markers: {type: [Marker], required: true}
 }, { versionKey: false, collection: 'read'})
