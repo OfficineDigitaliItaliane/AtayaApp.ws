607ea3c9a4d870557483df6633b3e33127c2b0a4
Giuseppe Zanini <giuseppe.zanini@mindtek.it>
2020-12-05 15:32:11 +0100

    feat: Added migrated record insertion

--- migrations/v2/old_read/old_read_to_json.js
+++ migrations/v2/old_read/old_read_to_json.js
@@ -1,22 +1,51 @@
 import fs from 'fs';
 
+import * as db from '../../../src/models/index'
+import mongoose from "mongoose";
+
 (async () => {
   console.log('Migration Started')
 
   const data = readAllOldData();
   for (const item of data) {
+
+    let id = 0;
+
     elementToMedia(item, ['picture'])
+    item['_id'] = item['id'];
+    console.log(item['_id'])
     if (!item['options']) {
       continue;
     }
+    item.markers = item.markers || [];
+    item['options'] = (item['options'] || {}).filter((option) => option.correct);
+
     for (const option of item['options']) {
+      option['_id'] = option['id'];
       elementToMedia(option, ['audio'])
-      delete option.correct
+      const newId = id++;
+      const marker = {
+        id: newId,
+        x: 0,
+        y: 0
+      }
+      item.markers.push(marker);
+      option.markerId = newId;
     }
   }
-  
+
+  const ids = data.map((i) => i['_id']);
+
+  await db.read.deleteMany({
+    _id:{
+      $in: ids
+    }
+  }).exec();
+  await db.read.insertMany(data);
+
   console.log(JSON.stringify(data, null, 4))
   console.log('Migration Ended')
+  mongoose.connection.close()
 })()
 
 
